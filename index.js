42ba1e4edd037cb4be660e1fd41350f7b786f195
Fred-riQ <fredrickbet5@gmail.com>
2024-10-30 14:03:23 +0300

    upload file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const drivers = ['Antonia','Nuru','Amari','Mo'];
+const returnFirstTwoDrivers = function(driver){return [...drivers].slice(null,2)};
+// console.log(returnFirstTwoDrivers(drivers))
+
+const returnLastTwoDrivers = function(drivers){return[...drivers].splice(2)};
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare){
+        return fare * multiplier;
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers,selectionFunction){
+    return selectionFunction(drivers)
+}
